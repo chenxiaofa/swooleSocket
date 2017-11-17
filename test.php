c47879b8f2a50df99d4206393f458b6b7c98604b
root <root@zzt>
2017-11-17 09:16:06 +0800

    add test.php

--- test.php
+++ test.php
@@ -1,31 +1,37 @@
 <?php
 
-class A
-{
+$client = new swoole_client(SWOOLE_SOCK_TCP);
+$client->connect('127.0.0.1',9502,-1);
+$array = ['path'=>'','params'=>''];
+$status = 1;
+while(true){
+	$content = read();	
 
-    public function __construct()
-    {
-        echo "开始一个类\n";
-    }
-
-    public function handel(){
-        echo "执行这个函数\n";
-    }
-
-    public function __destruct()
-    {
-        // TODO: Implement __destruct() method.
-        echo "结束一个类 \n";
-    }
-
+	if($content=='no'){
+		$client->send(json_encode($array));
+		$array = ['path'=>'','params'=>''];
+		$status = 1;		
+	}elseif($content=='path'){
+		$status = 1;
+	}elseif($content=='params'){
+		$status=2;
+	}else{
+		if($status==2){
+		$content = explode(":",$content);
+		$array['params'][$content[0]]=$content[1];
+		}else{
+		$array['path']=$content;
+		}
+	}
+	
+	fwrite(STDOUT,"has input : ".json_encode($array)."\n");
 }
 
 
-$a = new  A();
-$a->handel();
-for($i=0;$i<10;$i++){
-    echo "已经等待 $i.......\n";
+
+function read(){
+fwrite(STDOUT,"请输入参数：");
+return trim(fgets(STDIN));
 }
-echo "结束。\n";
 
 ?>
