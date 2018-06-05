86704711fdbd4de6ba8b0537a3801058885be05e
root <root@zzt>
2018-06-05 14:02:26 +0800

    記錄發送數據

--- Ext/Server.php
+++ Ext/Server.php
@@ -11,6 +11,7 @@ class Server
 {
     public static function send($data,$fd=0){
         if (self::fdExists($fd)){
+            echo "send Data:{$data} \n";
             $GLOBALS['serv']->send($fd!==0?$fd:$GLOBALS['fd'],$data);
         }else{
             echo "fd=>{$fd} is not found ! \n";
